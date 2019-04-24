ff21b11f123a7c85e40591d33ada56ee490260de
Parikshith <parikshithmohan@gmail.com>
2019-04-24 20:32:10 +0530

    first test

---
+++ src/cljs_test/core.cljs
@@ -0,0 +1,18 @@
+(ns cljs-test.core
+    (:require ))
+
+(enable-console-print!)
+
+
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "parikshith"
+                          :__figwheel_counter 0}))
+
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+   (swap! app-state update-in [:__figwheel_counter] inc)
+)
